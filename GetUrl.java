805046d54cec8e78f823d12c293a8c9bf0c328ce
Mike Holling <myke@fkspro.com>
2015-04-22 19:27:40 -0600

    stringbuilder instead of concat!

--- GetUrl.java
+++ GetUrl.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.StringBuilder;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.HttpURLConnection;
@@ -22,20 +23,20 @@ public class GetUrl {
 	
 	try {
 
-	    String line;
-	    String result = "";
-	    
 	    URL url = new URL(url_name);
 	    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
 	    BufferedReader result_reader = 
 		new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 										
+	    String line;
+	    StringBuilder result = new StringBuilder();
+	    
 	    while ((line = result_reader.readLine()) != null) {
-		result += line;
+		result.append(line);
 	    }
 
-	    return result;
+	    return result.toString();
 	
 	} catch (MalformedURLException e) {
 	    Log.e(TAG,Log.getStackTraceString(e));
